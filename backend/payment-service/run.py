978938ac78038b8b7d4fc24c7d4dde5be22d27f2
Noblette <noblette.tsimihanta@gmail.com>
2025-11-25 13:26:08 +0300

    localhost2

--- backend/payment-service/run.py
+++ backend/payment-service/run.py
@@ -7,4 +7,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(port=5004, debug=True, host='0.0.0.0')
+    app.run(port=5000, debug=True, host='0.0.0.0')
+    #app.run(port=5004, debug=True, host='0.0.0.0')#localhost
