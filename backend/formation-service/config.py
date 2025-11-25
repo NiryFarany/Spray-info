dd6d92c8def4403702d0745b6faa4325e8cc4860
Noblette <noblette.tsimihanta@gmail.com>
2025-11-25 11:58:42 +0300

    modification de locahost

--- backend/formation-service/config.py
+++ backend/formation-service/config.py
@@ -1,3 +1,4 @@
 #SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://username:password@localhost/formation_db"
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://SprayInfo:tsyHaiko#123@localhost/formation_db"
+#SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://SprayInfo:tsyHaiko#123@localhost/formation_db" #localhost
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://SprayInfo:tsyHaiko#123@formation_db/formation_db" 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
