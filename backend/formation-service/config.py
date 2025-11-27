54df185d1e9024ab1340e9c021cfeb1a0c14bfaa
Noblette <noblette.tsimihanta@gmail.com>
2025-11-27 10:57:15 +0300

    modif root pour docker

--- backend/formation-service/config.py
+++ backend/formation-service/config.py
@@ -1,4 +1,5 @@
 #SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://username:password@localhost/formation_db"
 #SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://SprayInfo:tsyHaiko#123@localhost/formation_db" #localhost
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://SprayInfo:tsyHaiko#123@formation_db/formation_db" 
+#SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://SprayInfo:tsyHaiko#123@formation_db/formation_db" #pour config root pour l'instant pour docker
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:root@formation_db/formation_db" #pour config root 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
