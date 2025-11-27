da8b8d84c0c838b67f4f38d5b0187b6316113601
Noblette <noblette.tsimihanta@gmail.com>
2025-11-27 09:52:28 +0300

    ajout de .env à la structure

--- Etat Frontend et backend/backend.py
+++ Etat Frontend et backend/backend.py
@@ -47,3 +47,55 @@ backend/
 │   ├── run.py
 structure backend
 '''
+
+'''
+backend/
+├── user-service/
+│   ├── app/
+│   │   ├── _init_.py
+│   │   ├── models/
+│   │   │   ├── _init_.py
+│   │   │   ├── user.py
+│   │   ├── routes/
+│   │   │   ├── _init_.py
+│   │   │   ├── user_routes.py
+│   ├── .env
+│   ├── config.py
+│   ├── run.py
+├── formation-service/
+│   ├── app/
+│   │   ├── _init_.py
+│   │   ├── models/
+│   │   │   ├── _init_.py
+│   │   │   ├── formation.py
+│   │   ├── routes/
+│   │   │   ├── _init_.py
+│   │   │   ├── formation_routes.py
+│   ├── config.py
+│   ├── run.py
+├── order-service/
+│   ├── app/
+│   │   ├── _init_.py
+│   │   ├── models/
+│   │   │   ├── _init_.py
+│   │   │   ├── order.py
+│   │   ├── routes/
+│   │   │   ├── _init_.py
+│   │   │   ├── order_routes.py
+│   ├── .env
+│   ├── config.py
+│   ├── run.py
+├── payment-service/
+│   ├── app/
+│   │   ├── _init_.py
+│   │   ├── models/
+│   │   │   ├── _init_.py
+│   │   │   ├── payment.py
+│   │   ├── routes/
+│   │   │   ├── _init_.py
+│   │   │   ├── payment_routes.py
+│   ├── .env
+│   ├── config.py
+│   ├── run.py
+
+'''
